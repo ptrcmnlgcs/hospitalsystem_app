f4df7053b36f646cc928a3639f19d31a44f91679
ptrcmnlgcs <gecosopatricia32@.gmail.com>
2024-06-13 02:10:12 +0800

    Add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,29 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import UserManagement from '@/components/Users.vue';
+import ManageRegister from '@/components/ManageRegister.vue'
 import ManageLogin from '@/components/ManageLogin.vue'
+
 
 const routes = [{
         path: '/',
         name: 'landing',
         component: ManageLogin,
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: ManageLogin,
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: ManageRegister,
+    },
+    {
+        path: '/users',
+        name: 'UserManagement',
+        component: UserManagement,
+        meta: { requiresAuth: true }
     },
     
 ]
