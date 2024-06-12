d1d3caf71bcaee8cda818e0a52d193e633cbc46f
haellyz28 <fornismarialeah891@gmail.com>
2024-06-13 02:48:54 +0800

    Add PopUpModal

---
+++ src/components/PopUpModal.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="modal-mask" @click.self="close">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+  
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+          </div>
+  
+          <div class="modal-footer">
+            <slot name="footer">
+              <button class="btn btn-primary" @click="close">
+                Close
+              </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'EditModal',
+    methods: {
+      close() {
+        this.$emit('close');
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+    transition: opacity 0.3s ease;
+  }
+  
+  .modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+  }
+  
+  .modal-container {
+    width: 500px;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+  
+  .modal-header, .modal-body, .modal-footer {
+    margin-bottom: 10px;
+  }
+  
+  .modal-footer {
+    text-align: right;
+  }
+
+
+  </style>
+  
