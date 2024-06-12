64672a8816a33d7b6ef597318a3637d032f01e7e
angellyn <guardianangellyn65@gmail.com>
2024-06-13 02:26:26 +0800

    add doctor user

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -2,7 +2,7 @@
     <DashboardView />
     <div class="hello">
         <h1>User Management</h1>
-        <button class="btn btn-info btn-sm m-1" @click="addUser()">Add a User</button>
+        <button class="btn btn-info btn-sm m-1" @click="addDoctor()">Add a User</button>
         <table class="table table-striped table-bordered">
             <thead class="thead-dark">
                 <tr>
@@ -58,3 +58,128 @@
             <div v-if="error" class="error">{{ error }}</div>
         </template>
     </Modal>
+    
+    <script>
+    import axios from 'axios';
+    import DashboardView from '@/views/DashboardView.vue';
+    
+    export default {
+        name: 'DoctorManagement',
+        components: {
+            Modal,
+            DashboardView
+        },
+        data() {
+            return {
+                users: [],
+                showAddUserModal: false,
+                showeditUserModal: false,
+                newUserData: {
+                    name: '',
+                    email: '',
+                    password: '',
+                    userType: ''
+                },
+                editUserData: {
+                    id: '',
+                    name: '',
+                    email: '',
+                    password: '',
+                    userType: '',
+                    updated_at: ''
+                }
+            };
+        },
+        mounted() {
+            this.fetchAllUsers();
+        },
+        methods: {
+            fetchAllUsers() {
+                fetch('http://127.0.0.1:8000/api/user', {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + localStorage.getItem('token')
+                        },
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        this.users = data.UserAccounts;
+                    })
+                    .catch(err => {
+                        if (err.response) {
+                            this.error = `Error: ${err.response.data.message}`;
+                            console.error(err.response.data);
+                        } else if (err.request) {
+                            this.error = 'No response from server. Please try again later.';
+                            console.error(err.request);
+                        } else {
+                            this.error = 'Request error. Please check your input and try again.';
+                            console.error('Error', err.message);
+                        }
+                    });
+            },
+            addDoctor() {
+                this.showAddUserModal = true;
+            },
+            async postUser() {
+                try {
+                    await axios.post('http://127.0.0.1:8000/api/user', this.newUserData, {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': 'Bearer ' + localStorage.getItem('token')
+                        }
+                    });
+                    this.showAddUserModal = false;
+                    this.newUserData = {
+                        name: '',
+                        email: '',
+                        password: '',
+                        userType: ''
+                    };
+    
+                } catch (error) {
+                    console.error('There was an error adding the product:', error);
+                }
+                this.fetchAllUsers();
+            },
+            editUser(user) {
+                this.editUserData = {
+                    ...user
+                };
+                this.showeditUserModal = true;
+            },
+            async updateUser() {
+                this.editUserData.updated_at = new Date().toISOString();
+                await axios.put(`http://127.0.0.1:8000/api/doctor/${this.editUserData.id}`, this.editUserData, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + localStorage.getItem('token')
+                    }
+                });
+                this.showeditUserModal = false;
+                this.fetchAllUsers();
+            },
+            async deleteUser(user) {
+                try {
+                    const userConfirmed = confirm('Are you sure you want to delete this user?');
+                    if (userConfirmed) {
+                        await axios.delete(`http://127.0.0.1:8000/api/doctor/${user.id}`, {
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Authorization': 'Bearer ' + localStorage.getItem('token')
+                            }
+                        });
+                        this.fetchAllUsers();
+                    } else {
+                        console.log('User deletion canceled.');
+                    }
+                } catch (error) {
+                    console.error('There was an error deleting the product:', error);
+                }
+            }
+    
+        }
+    };
+    </script>
+    
